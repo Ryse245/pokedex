6e6dcb7df94d9a7b17413035959828a4e695bc14
Ryse245 <bhubnerofficial@gmail.com>
2026-05-10 01:43:48 -0400

    pokeAPI JSON

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -19,12 +21,23 @@ func getCommands() map[string]clientCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays next Pokemon map data",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays previous Pokemon map data",
+			callback:    commandMapB,
+		},
 	}
 }
 
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
+	saveCommand := commandInfo{}
 
 	for {
 		fmt.Printf("Pokedex >")
@@ -34,23 +47,52 @@ func main() {
 
 		command, exists := getCommands()[clean[0]]
 		if exists {
-			command.callback()
+			command.callback(&saveCommand)
 		} else {
 			fmt.Printf("Unknown command\n")
 		}
 	}
 }
-func commandExit() error {
+func commandExit(config *commandInfo) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *commandInfo) error {
 	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n\n")
 	for _, command := range getCommands() {
 		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
+	return nil
+}
+
+func commandMap(config *commandInfo) error {
+	url := config.next
+	if url == "" {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	data := pokeAPIResponse{}
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		return err
+	}
+	for _, item := range data.Results {
+		fmt.Println(item.Name)
+	}
+	config.next = data.Next
+	config.previous = data.Previous
+
+	return nil
+}
+
+func commandMapB(config *commandInfo) error {
+	config.next = config.previous
+	commandMap(config)
 	return nil
 }
 
@@ -70,8 +112,13 @@ func cleanInput(text string) []string {
 	return ret
 }
 
+type commandInfo struct {
+	next     string
+	previous string
+}
+
 type clientCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*commandInfo) error
 }
